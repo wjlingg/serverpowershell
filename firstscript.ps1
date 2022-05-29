2e5c77d3383ad6d787e673962917114a2dab4f68
wjlingg <e0309488@u.nus.edu>
2022-05-29 16:09:27 +0800

    Able to take hostname input from a list of server in text file

--- firstscript.ps1
+++ firstscript.ps1
@@ -1,1 +1,12 @@
-Write-Host "Congratulations! Your first script executed successfully"
+Write-Host "Starting powershell script..."
+Write-Host "Getting information from a list of servers..."
+Write-Host "Below are all the available servers:"
+# get-content -Path "..\ServerList.txt" # kept the text file in home directory under script folder hence "..\" is used
+# get-childitem ENV: # returns a list of your environment variables
+# $ENV:COMPUTERNAME # returns the name of your computer
+$serverlist = @(get-content -Path "$env:HOMEDRIVE$env:HOMEPATH\ServerList.txt") 
+
+foreach ($server in $serverlist)
+{
+    write-host "Server: $server"
+} 
