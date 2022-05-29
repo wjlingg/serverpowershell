332394822cd913f355f18e24836316f2b11fb069
wjlingg <e0309488@u.nus.edu>
2022-05-29 21:25:07 +0800

    get from server list, return info

--- secondscript.ps1
+++ secondscript.ps1
@@ -1,10 +1,35 @@
-get-wmiobject win32_computersystem
-Get-WmiObject -class win32_processor | Select-Object NumberOfCores
-Get-ComputerInfo | Select-Object CsName, WindowsVersion, CsManufacturer, CsModel, CsPhyicallyInstalledMemory
-Get-WmiObject -class win32_logicaldisk | Select-Object @{n="Size (GB)";e={[math]::Round($_.Size/1GB,2)}}, @{n="FreeSpace (GB)";e={[math]::Round($_.FreeSpace/1GB,2)}}, @{n="Usage (GB)";e={[math]::Round(($_.Size-$_.FreeSpace)/1GB,2)}}, @{n="PercentUsage (%)";e={[math]::Round((($_.Size-$_.FreeSpace)/$_.Size)*100,2)}}
-Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object LastBootUpTime
-$bootuptime = (Get-CimInstance -ClassName Win32_OperatingSystem).LastBootUpTime
+$computername = (Get-CimInstance -ClassName Win32_ComputerSystem).Name
+$osversion = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").ReleaseId
+$lastbootime = (Get-CimInstance -ClassName Win32_OperatingSystem).LastBootUpTime
 $CurrentDate = Get-Date
 $uptime = $CurrentDate - $bootuptime
-$uptime
-Write-Output "Server Uptime --> Days: $($uptime.days), Hours: $($uptime.Hours), Minutes:$($uptime.Minutes)"
+$manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
+$model = (Get-CimInstance -ClassName Win32_ComputerSystem).Model
+$numberofcores = (Get-WmiObject -class win32_processor).NumberOfCores
+$memoryram = (Get-CimInstance -ClassName Win32_ComputerSystem ).totalphysicalmemory / 1GB
+$disksize = (Get-WmiObject -ComputerName $computername -class win32_logicaldisk).Size / 1GB
+$freespace = (Get-WmiObject -ComputerName $computername -class win32_logicaldisk).FreeSpace / 1GB
+$diskusage = ($disksize - $freespace) / $disksize * 100
+
+$serverlist = @(get-content -Path "$env:HOMEDRIVE$env:HOMEPATH\ServerList.txt") # get the content of the server name from server list
+$allServer = @()
+
+foreach ($server in $serverlist)
+{
+    write-host "Server: $server"
+    $serverObject = New-Object -TypeName psobject
+    $serverObject | Add-Member -MemberType NoteProperty -Name ComputerName -Value "$computername"
+    $serverObject | Add-Member -MemberType NoteProperty -Name OSVersion -Value "$osversion"
+    $serverObject | Add-Member -MemberType NoteProperty -Name Uptime -Value "Days: $($uptime.days), Hours: $($uptime.Hours), Minutes:$($uptime.Minutes)"
+    $serverObject | Add-Member -MemberType NoteProperty -Name LastRebootDetails -Value "$lastbootime"
+    $serverObject | Add-Member -MemberType NoteProperty -Name Manufacturer -Value "$manufacturer"
+    $serverObject | Add-Member -MemberType NoteProperty -Name Model -Value "$model"
+    $serverObject | Add-Member -MemberType NoteProperty -Name NumberOfCpuCores -Value "$numberofcores"
+    $serverObject | Add-Member -MemberType NoteProperty -Name MemoryRAM -Value "$([math]::Round($memoryram,6))GB"
+    $serverObject | Add-Member -MemberType NoteProperty -Name DiskCapacity -Value "$([math]::Round($disksize,6))GB"
+    $serverObject | Add-Member -MemberType NoteProperty -Name DiskUsage -Value "$([math]::Round($diskusage,6))%"
+
+    $allServer += $serverObject
+}
+
+$allServer | Export-Csv -Path "$env:HOMEDRIVE$env:HOMEPATH\ServerObjectListInfo.csv" # export object to csv file
